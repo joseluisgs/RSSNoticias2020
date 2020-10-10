a1ba5e98109fc08aeb119a752a6ee91569127333
joseluisgs <joseluisgonsan@gmail.com>
2020-10-10 10:28:33 +0200

    Inicio proyecto
    Inicio de Proyecto

---
+++ README.md
@@ -0,0 +1,18 @@
+# RSS Noticias
+
+2DAM PMYDM Sencillo lector de rss siguiendo Gitflow y RecyclerView
+
+[![Kotlin](https://img.shields.io/badge/Code-Kotlin-blue)](https://kotlinlang.org/)
+[![LISENCE](https://img.shields.io/badge/Lisence-MIT-red)]()
+
+## Autor
+
+- [José Luis González Sánchez](https://twitter.com/joseluisgonsan) ![Twitter](https://img.shields.io/twitter/follow/joseluisgonsan?style=social)
+
+- [GitHub](https://github.com/joseluisgs) ![GitHub](https://img.shields.io/github/followers/joseluisgs?style=social)
+
+![GitHub](https://img.shields.io/github/last-commit/joseluisgs/RSSKotlin2020)
+
+## Licencia
+
+Este proyecto esta licenciado bajo licencia **MIT**, si desea saber más, visite el fichero [LICENSE](https://github.com/joseluisgs/MoriaKotlin2020/blob/master/LISENCE) para su uso docente y educativo.
--- app/src/main/java/com/joseluisgs/rssnoticias/MainActivity.kt
+++ app/src/main/java/com/joseluisgs/rssnoticias/MainActivity.kt
@@ -16,11 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+                    .setAction("Action", null).show()
         }
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
@@ -30,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
+        return when(item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
