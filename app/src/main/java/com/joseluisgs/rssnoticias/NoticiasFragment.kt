5a050413f322ac482147292591e5896adc150c7b
joseluisgs <joseluisgonsan@gmail.com>
2020-10-10 20:06:02 +0200

    Noticias Modelo y Controllador

--- app/src/main/java/com/joseluisgs/rssnoticias/NoticiasFragment.kt
+++ app/src/main/java/com/joseluisgs/rssnoticias/NoticiasFragment.kt
@@ -11,14 +11,14 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class NoticiasFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_noticias, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
