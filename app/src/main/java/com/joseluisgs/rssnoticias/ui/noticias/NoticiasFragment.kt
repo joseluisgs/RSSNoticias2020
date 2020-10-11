7f70256c3402538a4f7e9c6e2c09f3b254c8aa6e
joseluisgs <joseluisgonsan@gmail.com>
2020-10-11 18:55:25 +0200

    Eventos Sweep Horizontal

--- app/src/main/java/com/joseluisgs/rssnoticias/ui/noticias/NoticiasFragment.kt
+++ app/src/main/java/com/joseluisgs/rssnoticias/ui/noticias/NoticiasFragment.kt
@@ -1,6 +1,7 @@
 package com.joseluisgs.rssnoticias.ui.noticias
 
 import android.app.Activity
+import android.graphics.*
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
@@ -9,7 +10,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.joseluisgs.rssnoticias.MainActivity
 import com.joseluisgs.rssnoticias.R
 import com.joseluisgs.rssnoticias.rss.Noticia
@@ -28,6 +32,7 @@ class NoticiasFragment : Fragment() {
     // Interfaz gráfica
     private lateinit var adapter: NoticiasListAdapter //Adaptador de Noticias de Recycler
     private lateinit var tarea: TareaCargarNoticias // Tarea en segundo plano
+    private var paintSweep = Paint()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -53,10 +58,9 @@ class NoticiasFragment : Fragment() {
         cargarNoticias()
 
         // solo si hemos cargado hacemos sl swipeHorizontal
-        // iniciarSwipeHorizontal();
+        iniciarSwipeHorizontal();
 
         // Mostramos las vistas de listas y adaptador asociado
-
         noticiasRecycler.layoutManager = LinearLayoutManager(context);
         Log.d("Noticias", "Asignado al RV");
     }
@@ -77,9 +81,119 @@ class NoticiasFragment : Fragment() {
      * Realiza el swipe horizontal si es necesario
      */
     private fun iniciarSwipeHorizontal() {
-        TODO("Not yet implemented")
+        val simpleItemTouchCallback: ItemTouchHelper.SimpleCallback = object : ItemTouchHelper.SimpleCallback(
+            0, ItemTouchHelper.LEFT or
+                    ItemTouchHelper.RIGHT
+        ) {
+            // Sobreescribimos los métodos
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Analizamos el evento según la dirección
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                // Si pulsamos a la de izquierda o a la derecha
+                // Programamos la accion
+                when (direction) {
+                    ItemTouchHelper.LEFT -> {
+                        // Log.d("Noticias", "Tocado izquierda");
+                        borrarElemento(position)
+                    }
+                    else -> {
+                        //  Log.d("Noticias", "Tocado derecha");
+                        editarElemento(position)
+                    }
+                }
+            }
+
+            // Dibujamos los botones y eveneto. Nos lo creemos :):)
+            // IMPORTANTE
+            // Para que no te explote las imagenes deben ser PNG
+            // Así que añade un IMAGE ASEET bjándtelos de internet
+            // https://material.io/resources/icons/?style=baseline
+            // como PNG y cargas el de mayor calidad
+            // de otra forma Bitmap no funciona bien
+            override fun onChildDraw(
+                canvas: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                    val itemView = viewHolder.itemView
+                    val height = itemView.bottom.toFloat() - itemView.top.toFloat()
+                    val width = height / 3
+                    // Si es dirección a la derecha: izquierda->derecta
+                    // Pintamos de azul y ponemos el icono
+                    if (dX > 0) {
+                        // Pintamos el botón izquierdo
+                        botonIzquierdo(canvas, dX, itemView, width)
+                    } else {
+                        // Caso contrario
+                        botonDerecho(canvas, dX, itemView, width)
+                    }
+                }
+                super.onChildDraw(canvas, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
+        }
+        // Añadimos los eventos al RV
+        val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(noticiasRecycler)
     }
 
+    /**
+     * Mostramos el elemento inquierdo
+     * @param canvas Canvas
+     * @param dX Float
+     * @param itemView View
+     * @param width Float
+     */
+    private fun botonDerecho(canvas: Canvas, dX: Float, itemView: View, width: Float) {
+        // Pintamos de rojo y ponemos el icono
+        paintSweep.color = Color.RED
+        val background = RectF(
+            itemView.right.toFloat() + dX,
+            itemView.top.toFloat(), itemView.right.toFloat(), itemView.bottom.toFloat()
+        )
+        canvas.drawRect(background, paintSweep)
+        val icon: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_sweep_eliminar)
+        val iconDest = RectF(
+            itemView.right.toFloat() - 2 * width, itemView.top.toFloat() + width, itemView.right
+                .toFloat() - width, itemView.bottom.toFloat() - width
+        )
+        canvas.drawBitmap(icon, null, iconDest, paintSweep)
+    }
+
+    /**
+     * Mostramos el elemento izquierdo
+     * @param canvas Canvas
+     * @param dX Float
+     * @param itemView View
+     * @param width Float
+     */
+    private fun botonIzquierdo(canvas: Canvas, dX: Float, itemView: View, width: Float) {
+        // Pintamos de azul y ponemos el icono
+        paintSweep.setColor(Color.BLUE)
+        val background = RectF(
+            itemView.left.toFloat(), itemView.top.toFloat(), dX,
+            itemView.bottom.toFloat()
+        )
+        canvas.drawRect(background, paintSweep)
+        val icon: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_sweep_detalles)
+        val iconDest = RectF(
+            itemView.left.toFloat() + width, itemView.top.toFloat() + width, itemView.left
+                .toFloat() + 2 * width, itemView.bottom.toFloat() - width
+        )
+        canvas.drawBitmap(icon, null, iconDest, paintSweep)
+    }
 
     /**
      * Carga las noticias
@@ -89,6 +203,36 @@ class NoticiasFragment : Fragment() {
         tarea?.execute(DIR_RSS)
     }
 
+    /**
+     * Acción primaria: Borra un elemento
+     * @param position Int
+     */
+    private fun borrarElemento(position: Int) {
+        Log.d("Noticias", "Borrar Elemento: $position")
+        // Acciones
+        val deletedModel = noticias[position]
+        adapter.removeItem(position)
+        // Mostramos la barra. Se la da opción al usuario de recuperar lo borrado con el el snackbar
+        val snackbar = Snackbar.make(view!!, "Noticia eliminada", Snackbar.LENGTH_LONG)
+        snackbar.setAction("DESHACER") { // undo is selected, restore the deleted item
+            adapter.restoreItem(deletedModel, position)
+        }
+        snackbar.setActionTextColor(resources.getColor(R.color.colorPrimary))
+        snackbar.show()
+    }
+
+    /**
+     * Acción secundaria: Ver/Editar
+     * @param position Int
+     */
+    private fun editarElemento(position: Int){
+        Log.d("Noticias", "Detalles Elemento: $position")
+        val noticia = noticias[position]
+        abrirNoticia(noticia)
+        // Esto es para que no se quede el color. Quitamos y ponemos la noticia
+        adapter.removeItem(position)
+        adapter.restoreItem(noticia, position);
+    }
 
     /**
      * Tarea asíncrona para la carga de noticias
@@ -155,16 +299,24 @@ class NoticiasFragment : Fragment() {
     private fun eventoClicFila(noticia: Noticia) {
         if((activity as MainActivity?)!!.isClicEventoFila) {
             Log.d("Noticias", "Has hecho clic en la noticia: $noticia")
-            val noticiaDetalle = NoticiaDetalleFragment(noticia)
-            val transaction = activity!!.supportFragmentManager.beginTransaction()
-            // animaciones
-//        transaction.setCustomAnimations(R.anim.animacion_fragment1,
-//        	R.anim.animacion_fragment1, R.anim.animacion_fragment2,
-//        	R.anim.animacion_fragment1)
-            //Llamamos al replace
-            transaction.replace(R.id.fragment_noticias, noticiaDetalle)
-            transaction.addToBackStack(null)
-            transaction.commit()
+            abrirNoticia(noticia)
         }
+    }
+
+    /**
+     * Abre una noticia como Fragment
+     * @param noticia Noticia
+     */
+    private fun abrirNoticia(noticia: Noticia) {
+        val noticiaDetalle = NoticiaDetalleFragment(noticia)
+        val transaction = activity!!.supportFragmentManager.beginTransaction()
+        // animaciones
+        //        transaction.setCustomAnimations(R.anim.animacion_fragment1,
+        //        	R.anim.animacion_fragment1, R.anim.animacion_fragment2,
+        //        	R.anim.animacion_fragment1)
+        //Llamamos al replace
+        transaction.replace(R.id.fragment_noticias, noticiaDetalle)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
