9957f9859e5bf92b00b72a58b5c48350414092c1
joseluisgs <joseluisgonsan@gmail.com>
2020-10-15 16:51:27 +0200

    Grafo de navegación acerca de

--- app/src/main/java/com/joseluisgs/rssnoticias/MainActivity.kt
+++ app/src/main/java/com/joseluisgs/rssnoticias/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.joseluisgs.rssnoticias.rss.Noticia
 import com.joseluisgs.rssnoticias.ui.acerca_de.AcercaDeFragment
 
@@ -71,6 +73,12 @@ class MainActivity : AppCompatActivity() {
 
 	private fun abrirAcercaDe() {
 		/**
+		 * Primera Forma:
+		 * crear el grafo de navegación y crear la acción
+		 */
+		Navigation.findNavController(this, R.id.fragment_noticias).navigate(R.id.action_noticiasFragment_to_acercaDeFragment)
+		/**
+		 * Segunda forma:
 		 * Transacción entre fragments. Lo primero es llamar al fragment manager
 		 * A continuación instanciamos un objeto fragment correspondiente al
 		 * fragment que va a entrar y le pasamos el objeto noticia en la posición de
@@ -78,16 +86,17 @@ class MainActivity : AppCompatActivity() {
 		 * animaciones. Lo más importante es el replace, en el que remplazamos el
 		 * host por el objeto fragment detalle
 		 */
-		val acercaDe = AcercaDeFragment()
-		val transaction = supportFragmentManager.beginTransaction()
-		// animaciones
-//		transaction.setCustomAnimations(R.anim.animacion_fragment1,
-//			R.anim.animacion_fragment1, R.anim.animacion_fragment2,
-//			R.anim.animacion_fragment1)
-		//Llamamos al replace
-		transaction.replace(R.id.nav_host_fragment, acercaDe)
-		transaction.addToBackStack(null)
-		transaction.commit()
+
+//		val acercaDe = AcercaDeFragment()
+//		val transaction = supportFragmentManager.beginTransaction()
+//		// animaciones
+////		transaction.setCustomAnimations(R.anim.animacion_fragment1,
+////			R.anim.animacion_fragment1, R.anim.animacion_fragment2,
+////			R.anim.animacion_fragment1)
+//		//Llamamos al replace
+//		transaction.replace(R.id.nav_host_fragment, acercaDe)
+//		transaction.addToBackStack(null)
+//		transaction.commit()
 		//Esto es para que no se quede con el color del deslizamiento
 
 	}
